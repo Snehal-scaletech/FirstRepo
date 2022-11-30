095f6583830debf2352d13f786ae3cbe05066ba2
Snehal-scaletech <snehal.helonde@scaletech.xyz>
2022-11-30 10:26:10 +0530

    Add security related apis

---
+++ src/app.controller.ts
@@ -0,0 +1,24 @@
+import { Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { AppService } from './app.service';
+import { AuthService } from './auth/auth.service';
+import { JwtAuthGuard } from './auth/jwt-auth.guard';
+import { LocalAuthGuard } from './auth/local-auth.guard';
+
+@Controller()
+export class AppController {
+  constructor(private authService : AuthService){}
+  @UseGuards(LocalAuthGuard)
+  @Post('auth/login')
+  
+  async login(@Request() req){
+    return await this.authService.login(req.user);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('profile')
+  getProfile(@Request() req)
+  {
+    return req.user;
+  }
+}
